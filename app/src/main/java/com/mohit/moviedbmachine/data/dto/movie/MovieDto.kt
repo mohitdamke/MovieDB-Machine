08f270c3e405f9997484155ce882f251324e2ac2
mohitdamke <mohitdamke1@gmail.com>
2026-07-16 14:47:28 +0530

    learning about movie dto and movie detail dto

--- app/src/main/java/com/mohit/moviedbmachine/data/dto/movie/MovieDto.kt
+++ app/src/main/java/com/mohit/moviedbmachine/data/dto/movie/MovieDto.kt
@@ -1,13 +1,6 @@
 package com.mohit.moviedbmachine.data.dto.movie
 
 data class MovieDto(
-//    val adult: Boolean?,
-//    val genre_ids: List<Int>?,
-//    val original_language: String?,
-//    val original_title: String?,
-//    val video: Boolean?,
-//    val vote_count: Int?
-//    val popularity: Double?,
     val id: Int?,
     val title: String?,
     val overview: String?,
